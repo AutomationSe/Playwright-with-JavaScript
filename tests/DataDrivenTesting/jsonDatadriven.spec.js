3d14dcf882e319038a13646ce7b312e092ee3d61
senel <senel.qa.emg@gmail.com>
2026-03-02 18:49:18 +0530

    add playwright report

--- tests/DataDrivenTesting/jsonDatadriven.spec.js
+++ tests/DataDrivenTesting/jsonDatadriven.spec.js
@@ -4,3 +4,16 @@ import fs  from 'fs';
 const testdatapath = 'Data/data.json'
 
 const testData = JSON.parse(fs.readFileSync(testdatapath, 'utf8'));
+
+for (const user of testData) {
+    test(`Login with user ${user.username}, ${user.password}`, async({page})=>{
+        test.slow();
+        await page.goto('https://www.saucedemo.com/')
+
+        await page.locator('#user-name').fill(user.username);
+        await page.locator('#password').fill(user.password);
+        await page.locator('[data-test="login-button"]').click();
+        await page.close();
+
+    })
+}
